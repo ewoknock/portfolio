fbb7b9d586a0d541a50965910c24ab8b3e5067c8
Enoch Hsu <1529929+ewoknock@users.noreply.github.com>
2024-05-26 16:16:00 -0700

    Adds in shopping cart project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
-import { useState } from 'react'
 import { BrowserRouter } from 'react-router-dom'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Navbar from './components/Navbar.jsx'
 import Hero from './components/Hero.jsx'
 import Skills from './components/Skills.jsx'
