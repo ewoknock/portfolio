87acaeec7702ea84172e833771508c9a4e4a7872
Enoch Hsu <1529929+ewoknock@users.noreply.github.com>
2024-05-26 11:04:14 -0700

    Adds todo png and updates project view

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
-# React + Vite
+# Portfolio website
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+This is a portfolio website built using Vite, React, and Bootstrap.
 
-Currently, two official plugins are available:
+- [Live Version](https://ewoknock.github.io/portfolio)
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+## Frameworks and Tools Used
+
+- React
+- Bootstrap
+- Node
+- Vite
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Tweeter from '../images/tweeter-splash.png'
 import Weather from '../images/weather-app.png'
+import ToDo from '../images/todo.png'
 import Restaurant from '../images/restaurant.png'
 import Booktracker from '../images/booktracker.png'
 import '../styles/Projects.css'
@@ -8,7 +9,7 @@ import '../styles/Projects.css'
 const ProjsData = [
     { key: 201, name: 'Tweeter', img: Tweeter, alt: 'Twitter Clone', desc: 'A twitter clone built using Ruby on Rails', github: 'https://github.com/ewoknock/tweeter', live: 'https://tweeter-twitterclone.fly.dev/' },
     { key: 202, name: 'Weather App', img: Weather, alt: 'Weather App', desc: 'A simple web app that utilizes API calls to get weather info and display it on the page.', github: 'https://github.com/ewoknock/odin-javascript-projects/tree/main/weather-app', live: 'https://ewoknock.github.io/odin-javascript-projects/weather-app/dist/index.html' },
-    { key: 203, name: 'To Do List', img: Tweeter, alt: 'To Do List', desc: 'A simple task and project organizer created with Javascript DOM manipulation.', github: 'https://github.com/ewoknock/odin-javascript-projects/tree/main/to-do-list', live: 'https://ewoknock.github.io/odin-javascript-projects/to-do-list/dist/index.html' },
+    { key: 203, name: 'To Do List', img: ToDo, alt: 'To Do List', desc: 'A simple task and project organizer created with Javascript DOM manipulation.', github: 'https://github.com/ewoknock/odin-javascript-projects/tree/main/to-do-list', live: 'https://ewoknock.github.io/odin-javascript-projects/to-do-list/dist/index.html' },
     { key: 204, name: 'Restaurant Page', img: Restaurant, alt: 'Restaurant Page', desc: 'A restaurant mockup page written primarily with Javascript DOM manipulation.', github: 'https://github.com/ewoknock/odin-javascript-projects/tree/main/restaurant-page', live: 'https://ewoknock.github.io/odin-javascript-projects/restaurant-page/dist/index.html' },
     { key: 205, name: 'Book Tracker', img: Booktracker, alt: 'Book Tracker', desc: 'A simple website that acts as a reading list. Built using Javascript, HTML, and CSS.', github: 'https://github.com/ewoknock/odin-javascript-projects/tree/main/library', live: 'https://ewoknock.github.io/odin-javascript-projects/library/index.html' },
   ];
